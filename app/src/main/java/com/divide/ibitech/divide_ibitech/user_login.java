f27905e7e45e329502a93d8ab0b79f75e8583a94
s216100801 <slabiti181@gmail.com>
2018-04-19 01:23:20 +0200

    login

---
+++ app/src/main/java/com/divide/ibitech/divide_ibitech/user_login.java
@@ -0,0 +1,32 @@
+package com.divide.ibitech.divide_ibitech;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class user_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+        final EditText ID_num= (EditText)findViewById(R.id.ID_Num);
+        final EditText Contact_Number= (EditText) findViewById(R.id.Contact_Number);
+        final EditText Password= (EditText) findViewById(R.id.Password);
+        final Button button_Login= (Button) findViewById(R.id.button_Login);
+
+        final TextView new_userRegister= (TextView) findViewById(R.id.new_userRegister);
+
+        new_userRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerInttent = new Intent(user_login.this,Register.class);
+                user_login.this.startActivity(registerInttent);
+            }
+        });
+    }
+}
