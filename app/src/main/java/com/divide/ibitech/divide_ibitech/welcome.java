3ae0d023910ffc116937b99833d23e4d55fb3b0c
lufefe <jumbaayabonga@gmail.com>
2018-04-21 18:13:08 +0200

    Added a welcome splash screen

--- app/src/main/java/com/divide/ibitech/divide_ibitech/welcome.java
+++ app/src/main/java/com/divide/ibitech/divide_ibitech/welcome.java
@@ -6,17 +6,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewParent;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class welcome extends AppCompatActivity {
-     private ViewPager mPager;
-     private    int [] layouts={R.layout.activity_slide_one,R.layout.activity_slide_two};
+    //private ViewPager mPager;
+    //private    int [] layouts={R.layout.activity_slide_one,R.layout.activity_slide_two};
+    LinearLayout topPart, bottomPart;
+    Button btnCont;
+    Animation uptodown,downtoup;
+    //private  MpagerAdapter mpagerAdapter;
 
-     private  MpagerAdapter mpagerAdapter;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-if (Build.VERSION.SDK_INT >= 19)
+        btnCont = (Button) findViewById(R.id.continueButton);
+        topPart = (LinearLayout) findViewById(R.id.topPart);
+        bottomPart = (LinearLayout) findViewById(R.id.bottomPart);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        topPart.setAnimation(uptodown);
+        bottomPart.setAnimation(downtoup);
+
+/*if (Build.VERSION.SDK_INT >= 19)
 
 {
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -31,6 +45,7 @@ getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS );
             mpagerAdapter = new MpagerAdapter(layouts,this);
             mPager.setAdapter(mpagerAdapter);
 
+    }*/
     }
 }
 
