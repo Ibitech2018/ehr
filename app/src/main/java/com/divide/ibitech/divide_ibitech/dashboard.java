e51d5a1ca351a470263fe9f184ef59383d13c973
lufefe <jumbaayabonga@gmail.com>
2018-05-21 04:35:48 +0200

    Pop up for manage condition

--- app/src/main/java/com/divide/ibitech/divide_ibitech/dashboard.java
+++ app/src/main/java/com/divide/ibitech/divide_ibitech/dashboard.java
@@ -1,7 +1,11 @@
 package com.divide.ibitech.divide_ibitech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class dashboard extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        LinearLayout condition = findViewById(R.id.manageCondition);
+
+        condition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(dashboard.this, Pop.class));
+            }
+        });
     }
 }
